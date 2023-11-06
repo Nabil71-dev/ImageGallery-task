f97c49e141f9b78ca4ff2ef8bf5dc0d38428da05
Nabil71-dev <mahmudnabiliiuc@gmail.com>
2023-11-06 20:18:24 +0600

    small changes

--- src/components/File.upload.jsx
+++ src/components/File.upload.jsx
@@ -12,7 +12,7 @@ const FileUpload = ({ addItem }) => {
     return (
         <div className="text-center file-upload-card p-3 bg-light rounded" {...getRootProps()}>
             <div>
-                <input {...getInputProps()} />
+                <input accept="image/png, image/jpg, image/jpeg" {...getInputProps()} />
                 <img className="img-upload" alt="File upload" src="https://i.pinimg.com/564x/04/54/7c/04547c2b354abb70a85ed8a2d1b33e5f.jpg" />
                 <p>Drag and drop files here or <strong>click to browse.</strong></p>
             </div>
